dbc564d11540abc844ed403b908615d2c0f81292
Daeholier <diegocisneros180@gmail.com>
2026-06-07 22:36:35 -0500

    Update client.py

--- Proyecto-Equipo1/client.py
+++ Proyecto-Equipo1/client.py
@@ -1,27 +1,22 @@
 import socket
 
-HOST = input("Ingresa la IP del servidor: ")
+HOST = "0.0.0.0"
 PORT = 5555
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 client.connect((HOST, PORT))
 
-mensaje = client.recv(1024).decode()
-
-print(mensaje)
+print(client.recv(1024).decode())
 
 while True:
+    num = input("Ingresa número: ")
 
-    intento = input("Ingresa tu número: ")
-
-    client.send(intento.encode())
+    client.send(num.encode())
 
     respuesta = client.recv(1024).decode()
-
     print(respuesta)
 
-    if "ganado" in respuesta.lower():
+    if respuesta == "Ganaste!":
         break
 
 client.close()
