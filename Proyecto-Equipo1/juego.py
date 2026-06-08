b21d652791b47dbf4615c938b8cf222ea53eafa4
Daeholier <diegocisneros180@gmail.com>
2026-06-07 22:03:15 -0500

    Update juego.py

--- Proyecto-Equipo1/juego.py
+++ Proyecto-Equipo1/juego.py
@@ -1,18 +1,19 @@
 print("=== JUEGO DE ADIVINAR EL NÚMERO ===")
 
-print("1. Iniciar Servidor")
-print("2. Iniciar Cliente")
+print("1. Modo Local")
+print("2. Iniciar Servidor")
+print("3. Iniciar Cliente")
 
 opcion = input("Seleccione una opción: ")
 
 if opcion == "1":
-
-    import server
+    import modo_local
 
 elif opcion == "2":
+    import server
 
+elif opcion == "3":
     import client
 
 else:
-
     print("Opción inválida")S
