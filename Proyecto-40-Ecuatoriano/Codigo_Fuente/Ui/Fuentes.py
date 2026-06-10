fa5612f1c6187be6aa18f50c2639eacb47a5006b
AlesVc <jovacapi@uide.edu.ec>
2026-06-10 12:12:32 -0500

    Add font initialization in Fuentes.py

---
+++ Proyecto-40-Ecuatoriano/Codigo_Fuente/Ui/Fuentes.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.font.init()
+
+fuente_titulo = pygame.font.SysFont("arialblack", 70)
+fuente_sub = pygame.font.SysFont("arial", 28)
+fuente_menu = pygame.font.SysFont("arial", 32)
+fuente_cartas = pygame.font.SysFont("arialblack", 32
