33de9f3743a4d791904301a971b53936b0f1ff77
Daeholier <diegocisneros180@gmail.com>
2026-06-11 08:14:10 -0500

    Update client.py

--- Proyecto-Equipo1/client.py
+++ Proyecto-Equipo1/client.py
@@ -1,22 +1,28 @@
 import socket
 
-HOST = "0.0.0.0"
+HOST = input("Ingrese la IP del servidor: ")
 PORT = 5555
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
 
-print(client.recv(1024).decode())
-
-while True:
-    num = input("Ingresa número: ")
-
-    client.send(num.encode())
-
-    respuesta = client.recv(1024).decode()
-    print(respuesta)
-
-    if respuesta == "Ganaste!":
-        break
+try:
+    client.connect((HOST, PORT))
+    print(client.recv(1024).decode())
+    while True:
+        try:
+            num = input("Ingresa número: ")
+            client.send(num.encode())
+            respuesta = client.recv(1024).decode()
+            if not respuesta:
+                print("Servidor desconectado")
+                break
+            print(respuesta)
+            
+            if respuesta == "Ganaste!":
+                break
+        except:
+            print("Servidor desconectado")
+            break
+except:
+    print("No se pudo conectar al servidor")
 
 client.close()
