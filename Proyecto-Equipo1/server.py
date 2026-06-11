9f56a6f02ac360829f9cb67901e6439e752bf268
Daeholier <diegocisneros180@gmail.com>
2026-06-11 08:15:43 -0500

    Update server.py

--- Proyecto-Equipo1/server.py
+++ Proyecto-Equipo1/server.py
@@ -9,11 +9,17 @@ print("1. Fácil")
 print("2. Medio")
 print("3. Difícil")
 
-nivel = int(input("Seleccione nivel: "))
+while True:
+    try:
+        nivel = int(input("Seleccione nivel: "))
+        if nivel >= 1 and nivel <= 3:
+            break
+        print("Seleccione un nivel válido")
+    except:
+        print("Ingrese un número válido")
 
 limite = seleccionar_limite(nivel)
 numero_secreto = generar_numero(limite)
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen(1)
@@ -23,24 +29,31 @@ print(f"Número generado entre 1 y {limite}")
 
 conn, addr = server.accept()
 print("Cliente conectado:", addr)
-
-conn.send(f"Adivina el número del 1 al {limite}".encode())
-
+conn.send(
+    f"Adivina el número del 1 al {limite}".encode()
+)
 while True:
-    data = conn.recv(1024).decode()
-
-    if not data:
+    try:
+        data = conn.recv(1024).decode()
+        if not data:
+            print("Cliente desconectado")
+            break
+        intento = int(data)
+        if intento == numero_secreto:
+            conn.send("Ganaste!".encode())
+            print("El cliente ganó")
+            break
+        elif intento < numero_secreto:
+            conn.send(
+                "El número es mayor".encode()
+            )
+        else:
+            conn.send(
+                "El número es menor".encode()
+            )
+    except:
+        print("Cliente desconectado")
         break
-
-    intento = int(data)
-
-    if intento == numero_secreto:
-        conn.send("Ganaste!".encode())
-        break
-    elif intento < numero_secreto:
-        conn.send("El número es mayor".encode())
-    else:
-        conn.send("El número es menor".encode())
 
 conn.close()
 server.close()
