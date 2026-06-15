a47e8de77849c8ea70ac11424f6d1839e15e75e3
AlesVc <jovacapi@uide.edu.ec>
2026-06-15 15:41:23 -0500

    Create CPU.py

---
+++ Proyecto Juego 40/Codigo_Fuente/Logica/CPU.py
@@ -0,0 +1,21 @@
+from logica import reglas40
+
+
+def jugar_cpu(juego):
+
+    mano = juego.mano_cpu
+    if not mano:
+        return 0
+
+    mejor_indice = None
+    mejor_cantidad = 0
+    for i, carta in enumerate(mano):
+        capturadas = reglas40.buscar_captura(juego.mesa, carta)
+        if len(capturadas) > mejor_cantidad:
+            mejor_cantidad = len(capturadas)
+            mejor_indice = i
+
+    if mejor_indice is not None:
+        return mejor_indice
+
+    return 0
