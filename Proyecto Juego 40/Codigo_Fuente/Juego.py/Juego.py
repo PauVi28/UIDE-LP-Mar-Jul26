3a005f8febc8fc4cdf0a8f0fbb2fcab473ac5ccc
AlesVc <jovacapi@uide.edu.ec>
2026-06-15 09:03:35 -0500

    Integrate multiplayer features and game state updates
    Added multiplayer functionality and improved game state management.

--- Proyecto Juego 40/Codigo_Fuente/Juego.py/Juego.py
+++ Proyecto Juego 40/Codigo_Fuente/Juego.py/Juego.py
@@ -1,8 +1,9 @@
 import pygame
+import time
 from ui.menu import mostrar_menu
 from ui.partida import mostrar_juego
 from ui.multijugador import mostrar_multijugador
-
+from multiplayer import estados
 from logica.game40 import Game40
 
 pygame.init()
@@ -17,6 +18,8 @@ clock = pygame.time.Clock()
 
 juego = Game40()
 estado = "menu"
+host_iniciado = False
+cliente=None
 
 ejecutando = True
 
@@ -48,19 +51,47 @@ while ejecutando:
 
             elif estado == "partida":
 
-                x_cartas = 180
-
-                for i in range(len(juego.mano_jugador)):
-
-                    if x_cartas <= x <= x_cartas + 120 and 500 <= y <= 670:
-
-                        juego.carta_seleccionada = i
-
-                        print("Carta seleccionada:", i)
-
-                    x_cartas += 140
-
                 if 430 <= x <= 810 and 560 <= y <= 630:
+
+                    print("BOTON JUGAR PRESIONADO")
+
+                    if juego.carta_seleccionada is not None:
+                        if estados.conectado:
+                            estados.cliente.enviar({
+                                "tipo": "jugada",
+                                "indice": juego.carta_seleccionada
+                            })
+                        else:
+                            juego.jugar_turno(juego.carta_seleccionada)
+
+                elif 40 <= x <= 420 and 620 <= y <= 690:
+
+                    estado = "menu"
+                else:
+
+                    x_cartas = 180
+
+                    for i in range(len(juego.mano_jugador)):
+
+                        if (
+                            x_cartas <= x <= x_cartas + 120
+                            and
+                            500 <= y <= 670
+                        ):
+
+                            juego.carta_seleccionada = i
+
+                            print(
+                                "Carta seleccionada:",
+                                i
+                            )
+
+                            break
+
+                        x_cartas += 140
+         
+                if 430 <= x <= 810 and 400 <= y <= 470:
+                    print("BOTON JUGAR PRESIONADO")
 
                     if juego.carta_seleccionada is not None:
 
@@ -71,8 +102,40 @@ while ejecutando:
                     estado = "menu"
 
             elif estado == "multijugador":
+                
+                if 420 <= x <= 800 and 250 <= y <= 320:
 
-                if 40 <= x <= 420 and 600 <= y <= 670:
+                    print("CREAR PARTIDA")
+
+                    from multiplayer.host import iniciar_host
+                    if not host_iniciado:
+                        iniciar_host()
+                        time.sleep(1)
+                        estados.es_host = True
+                        host_iniciado = True
+                        print("HOST INICIADO")
+            
+                elif 420 <= x <= 800 and 350 <= y <= 420:
+
+                    print("UNIRSE A PARTIDA")
+
+                    from network.cliente import ClienteLAN
+                    try:
+                        cliente = ClienteLAN("127.0.0.1")
+                        from multiplayer import estados
+                        estados.cliente=cliente
+                        estados.conectado=True
+                        print("CONECTADO")
+                        cliente.enviar({
+                            "tipo": "mensaje",
+                            "texto": "Hola desde LAN"
+                        })
+
+                        print("MENSAJE ENVIADO")
+                    except Exception as e:
+                        print("ERROR",e)
+               
+                elif 40 <= x <= 420 and 600 <= y <= 670:
                     estado = "menu"
 
     if estado == "menu":
@@ -86,8 +149,34 @@ while ejecutando:
     elif estado == "multijugador":
 
         mostrar_multijugador(pantalla, juego)
+    fuente = pygame.font.SysFont(
+        None,
+        60
+    )
+    if juego.ganador is not None:
 
+        texto = fuente.render(
+            f"GANADOR: {juego.ganador}",
+            True,
+            (255, 255, 0)
+        )
+
+        pantalla.blit(
+            texto,
+            (300, 200)
+        )
+
+    if cliente is not None:
+
+        while len(cliente.mensajes) > 0:
+
+            mensaje = cliente.mensajes.pop(0)
+            if mensaje["tipo"] == "jugada":
+                print("JUGADA RECIBIDA:", mensaje["indice"])
+
+            print("RECIBIDO:", mensaje)
     pygame.display.update()
+
 
     clock.tick(60)
 
