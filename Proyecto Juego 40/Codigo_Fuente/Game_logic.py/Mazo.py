a883e9a827860b52fb8b739944d63ea53ac817d1
AlesVc <jovacapi@uide.edu.ec>
2026-06-15 15:32:12 -0500

    Refactor card deck creation and value representation

--- Proyecto Juego 40/Codigo_Fuente/Game_logic.py/Mazo.py
+++ Proyecto Juego 40/Codigo_Fuente/Game_logic.py/Mazo.py
@@ -1,21 +1,23 @@
 import random
 
+PALOS = ["oros", "copas", "espadas", "bastos"]
+VALORES = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12]  
+
+
 def crear_mazo():
-
-    palos = ["♠️", "♥️", "♦️", "♣️"]
-
-    valores = [
-        "A", "2", "3", "4", "5",
-        "6", "7", "8", "9", "10",
-        "J", "Q", "K"
-    ]
-
     mazo = []
-
-    for palo in palos:
-        for valor in valores:
-            mazo.append(valor + palo)
-
+    for palo in PALOS:
+        for valor in VALORES:
+            carta = {"valor": valor, "palo": palo}
+            mazo.append(carta)
     random.shuffle(mazo)
-
     return mazo
+
+
+def texto_valor(valor):
+    especiales = {1: "A", 10: "S", 11: "C", 12: "R"}
+    return especiales.get(valor, str(valor))
+
+
+def nombre_carta(carta):
+    return texto_valor(carta["valor"]) + " de " + carta["palo"]
